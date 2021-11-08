86aa8cee7aec22cb8906dbf6369be52421193992
unknown <diwan.purnama.tif20@polban.ac.id>
2021-11-08 13:01:58 +0700

    add service cart

--- service/service.go
+++ service/service.go
@@ -1,11 +1,36 @@
 package service
 
 import (
-	fmt "fmt"
+	"context"
+	"log"
+
+	"github.com/go-kit/kit/log"
 )
 
-func HelloWorld(name string) string {
-	var helloOutput string
-	helloOutput = fmt.Sprintf("Hello, %s ", name)
-	return helloOutput
+type(
+	Service interface{
+		addCart(ctx context.Context,id int, quantity int,variant string) (string, error)
+	}
+	service  struct{
+		repository datastruct.DBrepository
+		logger log.Logger
+	}
+)
+
+func NewService(repo datastruct.AddCartRepository, logger log.Logger) Service{
+	return &service{
+		repository: repo,
+		logger: log.With(logger, "repo", "service"),
+	}
+}
+
+func (s *service) AddCart(ctx context.Context,id int, quantity int, variant string) (string,error){
+	logger := log.With(s.logger, "method", "AddCart")
+	if err := s.repository.AddCart(ctx,); err != nil{
+		level.Error(logger).Log("err",err)
+		return "", err
+	}
+
+	logger.Log("create user",id)
+	return "Success",nil
 }
