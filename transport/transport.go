5d73cd62eb4d4ad31c347d52a248a496363ee21f
unknown <gina.anifah.tif20@polban.ac.id>
2022-01-02 06:59:58 +0700

    menampilkan cart per user

--- transport/transport.go
+++ transport/transport.go
@@ -20,6 +20,13 @@ import (
 	"github.com/subchen/go-curl"
 )
 
+// const (
+// 	host     = "103.157.96.115"
+// 	port     = 5432
+// 	user     = "rantaipolygon"
+// 	password = "whirlpool"
+// 	dbname   = "db_rantaipolygon"
+// )
 
 const (
 	layoutDateTime = "2006-01-02 15:04:05"
@@ -37,7 +44,15 @@ var crt interface{}
 
 var db *sql.DB
 
-
+// type carts struct {
+// 	cart_id          int
+// 	status           string
+// 	checkout_date    string
+// 	payment_date     string
+// 	user_id          int
+// 	transaction_code string
+// 	payment_method   string
+// }
 
 type AphService interface {
 	HelloWorldService(context.Context, string) string
@@ -103,6 +118,14 @@ func RegisterHttpsServicesAndStartListener() {
 	http.Handle("/HelloWorld", HelloWorldHandler)
 }
 
+// func Response_Show_Carts(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Println("Tes show doang")
+// }
+
+// func Handle_Show_Carts() {
+// 	myRouter := mux.NewRouter().StrictSlash(true)
+// 	myRouter.HandleFunc("Carts", Response_Show_Carts).Methods("GET")
+// }
 
 func Text() {
 	fmt.Println("Hello")
@@ -145,7 +168,10 @@ func ConnDB() (*sql.DB, error) {
 	db, err := sql.Open("postgres", connStr)
 	checkerror(err)
 	fmt.Println("Database Succesfully Connected")
-	
+	//close databse
+	//defer db.Close()
+
+	//check database
 	err = db.Ping()
 	checkerror(err)
 	if err != nil {
@@ -155,6 +181,210 @@ func ConnDB() (*sql.DB, error) {
 	return db, nil
 }
 
+func Delete(ctx context.Context, cart datastruct.CartsFields) error {
+	db, err := ConnDB()
+
+	if err != nil {
+		fmt.Sprintf("cant connect to database")
+	}
+
+	queryText := fmt.Sprintf("delete from carts where cart_id = '%d'", cart.Cart_Id)
+
+	s, err := db.ExecContext(ctx, queryText)
+
+	if err != nil && err != sql.ErrNoRows {
+		return err
+	}
+
+	check, err := s.RowsAffected()
+	fmt.Println(check)
+	if check == 0 {
+		return errors.New("id tidak ada")
+	}
+	return nil
+}
+
+func DeleteMahasiswa(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "DELETE" {
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		var cart datastruct.CartsFields
+		id := r.URL.Query().Get("id")
+		if id == "" {
+			ResponseJSON(w, "id tidak boleh kosong", http.StatusBadRequest)
+			return
+		}
+		cart.Cart_Id, _ = strconv.Atoi(id)
+
+		if err := Delete(ctx, cart); err != nil {
+			kesalahan := map[string]string{
+				"error": fmt.Sprintf("%v", err),
+			}
+			ResponseJSON(w, kesalahan, http.StatusInternalServerError)
+			return
+		}
+
+		http.Error(w, "Tidak di ijinkan", http.StatusMethodNotAllowed)
+		return
+
+	}
+}
+
+func ShowCarts(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		ctx, cancel := context.WithCancel(context.Background())
+
+		defer cancel()
+
+		carts, err := GetAll(ctx)
+
+		if err != nil {
+			kesalahan := map[string]string{
+				"error": fmt.Sprintf("%v", err),
+			}
+
+			ResponseJSON(w, kesalahan, http.StatusInternalServerError)
+			return
+		}
+
+		ResponseJSON(w, carts, http.StatusOK)
+		return
+	}
+
+	http.Error(w, "Tidak di ijinkan", http.StatusMethodNotAllowed)
+	return
+}
+
+// GetAll
+func GetAll(ctx context.Context) ([]datastruct.CartsFields, error) {
+
+	var carts []datastruct.CartsFields
+
+	db, err := ConnDB()
+
+	if err != nil {
+		fmt.Print("Cannt connect database", err)
+	}
+
+	queryText := ("SELECT * FROM carts Order By cart_id DESC")
+
+	rowQuery, err := db.QueryContext(ctx, queryText)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for rowQuery.Next() {
+		var cart datastruct.CartsFields
+		//var Checkout_Date, Payment_Date string
+
+		if err = rowQuery.Scan(
+			&cart.Cart_Id,
+			&cart.Status,
+			&cart.Checkout_Date,
+			&cart.Payment_Date,
+			&cart.User_Id,
+			&cart.Transaction_Code,
+			&cart.Payment_Method); err != nil && sql.ErrNoRows != nil {
+			return nil, err
+		}
+
+		//  Change format string to datetime for created_at and updated_at
+		//	cart.Checkout_Date, err = time.Parse(layoutDateTime, Checkout_Date)
+
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+
+		// cart.Payment_Date, err = time.Parse(layoutDateTime, Payment_Date)
+
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+
+		carts = append(carts, cart)
+	}
+
+	return carts, nil
+}
+
+/*Insert to Cart and Order Items*/
+
+func PostCart(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+
+		if r.Header.Get("Content-Type") != "application/json" {
+			http.Error(w, "Gunakan content type application / json", http.StatusBadRequest)
+			return
+		}
+
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		var cart datastruct.CartsFields
+		//	var order_item datastruct.OrderItemsFields
+
+		if err := json.NewDecoder(r.Body).Decode(&cart); err != nil {
+			ResponseJSON(w, err, http.StatusBadRequest)
+			return
+		}
+		//	, order_item
+		if err := InsertCartTable(ctx, cart); err != nil {
+			ResponseJSON(w, err, http.StatusInternalServerError)
+			return
+		}
+
+		res := map[string]string{
+			"status": "Succesfully",
+		}
+
+		ResponseJSON(w, res, http.StatusCreated)
+
+		return
+	}
+
+	http.Error(w, "Tidak di ijinkan", http.StatusMethodNotAllowed)
+	return
+}
+
+func InsertCartTable(ctx context.Context, cart datastruct.CartsFields) error {
+	//, ortem datastruct.OrderItemsFields
+	db, err := ConnDB()
+
+	if err != nil {
+		fmt.Printf("cannot connect db")
+	}
+	/*%v bisa untuk segala jenis data*/
+
+	query := fmt.Sprintf("insert into carts (status,checkout_date,payment_date,user_id,transaction_code,payment_method) values ('%v','%v','%v',%v,%v,'%v')",
+		cart.Status,
+		cart.Checkout_Date,
+		cart.Payment_Date,
+		cart.User_Id,
+		cart.Transaction_Code,
+		cart.Payment_Method)
+
+	_, err = db.ExecContext(ctx, query)
+
+	if err != nil && err != sql.ErrNoRows {
+		return err
+	}
+
+	return nil
+}
+
+func GetDataProduk(w http.ResponseWriter, r *http.Request) {
+	req := curl.NewRequest()
+	req.Method = "GET"
+	req.URL = "https://617e57972ff7e600174bd77c.mockapi.io/api/carts/Carts/:id/products"
+	resp, err := req.Do()
+	if err != nil {
+		log.Fatalln("Unable to request ", err)
+	}
+	fmt.Println(resp.Text())
+
+}
 
 /*============================ MEMASUKKAN ITEM KEDALAM CART =========================================*/
 func AddProductToCart(w http.ResponseWriter, r *http.Request) {
@@ -664,7 +894,6 @@ func DeletePerProductFromCart(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-// Update
 func DeletePerProductFromCartSql(ctx context.Context, oritem datastruct.OrderItemsFields) error {
 
 	db, err := ConnDB()
@@ -824,4 +1053,86 @@ func CheckoutSql(ctx context.Context, cart datastruct.CartsFields) error {
 	return nil
 }
 
-/*===============================================================================================================*/
+/*=======================================================================================================*/
+/*================================= Menampilkan daftar cart =============================================*/
+func GetCartUser(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		ctx, cancel := context.WithCancel(context.Background())
+
+		defer cancel()
+		var id int
+		iduser := r.URL.Query().Get("iduser")
+
+		if iduser == "" {
+			ResponseJSON(w, "id tidak boleh kosong", http.StatusBadRequest)
+			return
+		}
+
+		id, _ = strconv.Atoi(iduser)
+
+		crt, err := GetAllCartUser(ctx, id)
+
+		if err != nil {
+			kesalahan := map[string]string{
+				"error": fmt.Sprintf("%v", err),
+			}
+
+			ResponseJSON(w, kesalahan, http.StatusInternalServerError)
+			return
+		}
+
+		ResponseJSON(w, crt, http.StatusOK)
+		return
+	}
+
+	http.Error(w, "Tidak di ijinkan", http.StatusMethodNotAllowed)
+	return
+}
+
+func GetAllCartUser(ctx context.Context, iduser int) ([]datastruct.MixCartOrder, error) {
+
+	var carts []datastruct.MixCartOrder
+
+	db, err := ConnDB()
+
+	if err != nil {
+		log.Fatal("Cant connect to Database", err)
+	}
+
+	queryText := fmt.Sprintf("select * from v_cartsandproduct where id_user = '%d'", iduser)
+
+	rowQuery, err := db.QueryContext(ctx, queryText)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rowQuery.Next() {
+		var cart datastruct.MixCartOrder
+
+		if err = rowQuery.Scan(
+			&cart.Cart_Id,
+			&cart.User_Id,
+			&cart.Status,
+			&cart.Oritem_id,
+			&cart.Product_Id,
+			&cart.Qty,
+			&cart.Color,
+			&cart.Psize,
+			&cart.Store_Id); err != nil && sql.ErrNoRows != nil {
+			return nil, err
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		carts = append(carts, cart)
+	}
+
+	return carts, nil
+}
